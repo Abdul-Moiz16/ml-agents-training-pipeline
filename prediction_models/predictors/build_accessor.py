99d553c6c5517d14cd52537549cccdd346ac5e52
Pawel Szybiak <142670872+Pawelomelo@users.noreply.github.com>
2026-01-18 15:25:35 +0100

    minor tweak to print in build_accessor.py

--- prediction_models/predictors/build_accessor.py
+++ prediction_models/predictors/build_accessor.py
@@ -84,9 +84,11 @@ def main():
         results[f"Predicted_{target}"] = pred
         # TODO check if works correctly since untested (save to predicted_runs folder)
         yaml_name = Path(yaml_path).stem
-        results.to_csv(Path(__file__).resolve().parents[1] / f"predicted_runs/predicted_{yaml_name}.csv", index=False)
+        pred_path = Path(__file__).resolve().parents[1] / f"predicted_runs/predicted_{yaml_name}.csv"
+        results.to_csv(pred_path, index=False)
 
         print(f"Predicted: {pred}")
+        print(f"Saved to: {pred_path}")
 
     else:
         print("Usage example: python build_accessor.py avg_ram_usage as argv[1]")
